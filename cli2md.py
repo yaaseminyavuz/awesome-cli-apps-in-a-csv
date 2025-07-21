01e79950cda2895225b5ea94191b0e4879c6ced6
yasemin <yaaseminyavuz@gmail.com>
2025-07-21 20:13:57 +0200

    Update cli2md.py

--- cli2md.py
+++ cli2md.py
@@ -1,7 +1,8 @@
 import csv
 import string
+from collections import defaultdict
 
-
+# === Templates ===
 summary_template = """
 Summary:
 
@@ -10,7 +11,20 @@ Summary:
 
 # Contents
 
-{cats}
+{toc}
+"""
+
+subcategory_section_template = """
+## 📁 {subcategory_label}
+{subcategory_description}
+
+{apps}
+"""
+
+category_section_template = """
+# {category_label}
+
+{subcategories}
 """
 
 resources_template = """
@@ -18,105 +32,94 @@ resources_template = """
 
 A list of some online resources that contribute interesting links to apps and info.
 
-{}
+{resources}
 """
 
-
+# === Loaders ===
 def load_csv(file_name):
-    with open(file_name, 'r') as infile:
-        csv_reader = csv.DictReader(infile, delimiter=',')
-        fields = csv_reader.fieldnames
-        data = []
-        for item in csv_reader:
-            data.append(item)
-    return fields, data
+    with open(file_name, 'r', encoding='utf-8') as infile:
+        return list(csv.DictReader(infile))
 
-
+# === Formatters ===
 def fmt_app(app):
-    descr = ''.join(c if c != '\n' else ' ' for c in app['description'])
-    if app['homepage'] != '':
-        st = '* [{}]({}) - {}'.format(app['name'], app['homepage'], descr)
-    elif app['git'] != '':
-        st = '* [{}]({}) - {}'.format(app['name'], app['git'], descr)
-    else:
-        st = '* {} - {}'.format(app['name'], descr)
-    return st
+    descr = app['description'].replace('\n', ' ').strip()
+    name = app['name']
+    link = app['homepage'] if app['homepage'].startswith('http') else app['git']
+    if not link:
+        return f"* {name} - {descr}"
+    return f"* [{name}]({link}) - {descr}"
 
+def fmt_resource(res):
+    return f"[{res['title']}]({res['url']}) - {res['description']}"
 
-def print_apps(cats, apps):
-    for c in cats:
-        cat_item = cats[c]
-        print('## <a name="{}"></a>{}\n'.format(c, cat_item['name']))
-        if cat_item['description'] != '':
-            print(f"{cat_item['description']}.\n")
-        apps_in_cat = [a for a in apps if a['category'] == c]
-        apps_in_cat = sorted(apps_in_cat, key=lambda i: i['name'].upper())
-        for app in apps_in_cat:
-            print(fmt_app(app))
-        print()
+def github_anchor(text):
+    import re
+    text = text.strip().lower()
+    text = re.sub(r'[^\w\s-]', '', text)
+    text = re.sub(r'\s+', '-', text)
+    return '-' + text
 
+def fmt_toc(categories):
+    toc_lines = []
+    for cat in categories:
+        toc_lines.append(f"### {cat['category_label']}")
+        for sub in cat['subcategories']:
+            anchor = github_anchor(sub['subcategory_label'])
+            toc_lines.append(f"- [{sub['subcategory_label'].replace('-', ' ').title()}](#{anchor}): {sub['subcategory_description']}")
+        toc_lines.append("")
+    return '\n'.join(toc_lines)
 
-def fmt_categories(cats):
-    group_by_letters = {x: [] for x in string.ascii_uppercase}
-    for c in cats:
-        initial = cats[c]['name'][0].upper()
-        group_by_letters[initial].append(c)
-    # print(group_by_letters)
-    strings_by_letters = {x: [] for x in string.ascii_uppercase}
-    for g in group_by_letters:
-        strings_by_letters[g] = [f"[{cats[c]['name']}](#{c}) ({cats[c]['count']})" for c in group_by_letters[g]]
-    lines_by_letters = {x: [] for x in string.ascii_uppercase}
-    # print(strings_by_letters)
-    for g in group_by_letters:
-        lines_by_letters[g] = ', '.join(strings_by_letters[g])
-    lines_to_join = ['* ' + lines_by_letters[key] for key in lines_by_letters if len(lines_by_letters[key])]
-    # print(lines_by_letters)
-    # print(lines_to_join)
-    return '\n'.join(lines_to_join)
+# === Main Markdown Generator ===
+def generate_readme(apps, cleaned_categories, resources):
+    # Organize subcategories under categories
+    grouped = defaultdict(lambda: {'category_label': '', 'subcategories': []})
+    for row in cleaned_categories:
+        cat = row['category_name']
+        grouped[cat]['category_label'] = row['category_label']
+        grouped[cat]['subcategories'].append({
+            'subcategory_name': row['subcategory_name'],
+            'subcategory_label': row['subcategory_label'],
+            'subcategory_description': row['subcategory_description']
+        })
 
+    # Prepare TOC
+    toc = fmt_toc(grouped.values())
 
-def fmt_categories_old(cats):
-    st = []
-    for c in cats:
-        st.append("[{}](#{}) ({})".format(cats[c]['name'], c, cats[c]['count']))
-    return ', '.join(st)
+    # Prepare content sections
+    content_lines = []
+    for cat_data in grouped.values():
+        sub_lines = []
+        for sub in cat_data['subcategories']:
+            apps_in_sub = [a for a in apps if a['Category'] == cat_data['category_label'] and a['Subcategory'] == sub['subcategory_name']]
+            if not apps_in_sub:
+                continue
+            app_lines = [fmt_app(app) for app in sorted(apps_in_sub, key=lambda x: x['name'].lower())]
+            sub_block = subcategory_section_template.format(
+                subcategory_label=sub['subcategory_label'],
+                subcategory_description=sub['subcategory_description'],
+                apps='\n'.join(app_lines)
+            )
+            sub_lines.append(sub_block)
 
+        if sub_lines:
+            content_lines.append(category_section_template.format(
+                category_label=cat_data['category_label'],
+                subcategories='\n'.join(sub_lines)
+            ))
 
-def count_apps(apps, categories):
-    for c in categories:
-        categories[c]['count'] = 0
-    for a in apps:
-        c = a['category']
-        if c in categories:
-            categories[c]['count'] += 1
-        # else:
-        #     print('Category {} does not have any app'.format(c))
-    return categories
+    # Prepare resources section
+    resources_section = ""
+    if resources:
+        resource_lines = [fmt_resource(r) for r in resources]
+        resources_section = resources_template.format(resources='\n\n'.join(resource_lines))
 
+    # Combine all sections
+    summary = summary_template.format(n_apps=len(apps), n_cats=len(grouped), toc=toc)
+    return summary + '\n\n' + '\n'.join(content_lines) + '\n\n' + resources_section
 
-def categories_list_to_dict(category_list):
-    category_list = sorted(category_list, key=lambda d: d['name'])
-    d = {}
-    for c in category_list:
-        d[c['label']] = {'name': c['name'], 'description': c['description']}
-    return d
-
-
-def main():
-    _, apps = load_csv('data/apps.csv')
-    _, categories = load_csv('data/categories.csv')
-    _, resources = load_csv('data/resources.csv')
-    categories = categories_list_to_dict(categories)
-    categories = count_apps(apps, categories)
-
-    md_res = ''
-    for r in resources:
-        md_res += '[{}]({}) - {}\n\n'.format(r['title'], r['url'], r['description'])
-
-    print(summary_template.format(n_apps=len(apps), n_cats=len(categories), cats=fmt_categories(categories)))
-    print_apps(categories, apps)
-    print(resources_template.format(md_res))
-
-
-if __name__ == '__main__':
-    main()
+# === Example Usage ===
+# apps = load_csv('apps.csv')
+# cleaned_categories = load_csv('compact_categories_subcategories_cleaned.csv')
+# resources = load_csv('resources.csv')
+# markdown = generate_readme(apps, cleaned_categories, resources)
+# print(markdown)
